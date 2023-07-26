e90b10326de2a81cee1f39edcb62f1ffaaae5a71
VickyMontanezCampus <vmontanez707@gmail.com>
2023-07-25 19:13:28 -0500

    SISEPUDOOO

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-import "reflect-metadata";
-import {plainToClass} from "class-transformer";
-import {user} from "./controller/user.js";
-
-
-
-let data = plainToClass(user, json, {excludeExtraneousValues: true});
-console.log(data.nombreId);
-A<ZSXDCFVGHBNJMK,LÑ.
+console.log("SISEPUDOOO");
