d2acf1c96ad04dee80de9ad380325649387422ce
VickyMontanezCampus <vmontanez707@gmail.com>
2023-07-25 19:11:40 -0500

    SI SIRVEEEE

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ import "reflect-metadata";
 import {plainToClass} from "class-transformer";
 import {user} from "./controller/user.js";
 
-let json = {
-    id: 12666,
-    nombre: "vickysiitaa",
-    edad: 17
-};
+
 
 let data = plainToClass(user, json, {excludeExtraneousValues: true});
 console.log(data.nombreId);
+A<ZSXDCFVGHBNJMK,LÑ.
